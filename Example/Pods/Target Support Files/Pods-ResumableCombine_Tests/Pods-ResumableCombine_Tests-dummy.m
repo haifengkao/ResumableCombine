bfefc7303385694d8a110d5a6a3d07292ed2a213
Hai Feng Kao <haifeng@cocoaspice.in>
2020-09-11 21:33:08 +0800

    initial commit

---
+++ Example/Pods/Target Support Files/Pods-ResumableCombine_Tests/Pods-ResumableCombine_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ResumableCombine_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_ResumableCombine_Tests
+@end
---
+++ Example/Pods/Target Support Files/ResumableCombine/ResumableCombine-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ResumableCombine : NSObject
+@end
+@implementation PodsDummy_ResumableCombine
+@end
